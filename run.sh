2b14fd13cc8307761841ad80392749e97c5bfe29
Ricky Nelson <ricky.m.nelson@gmail.com>
2018-05-03 00:31:27 -0400

    fine tuning of run.sh and tasks

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-sudo yum -y install ansible
-sudo yum -y install git
+yum -y install ansible
+yum -y install git
 
 mkdir /home/rnelson/git
 cd /home/rnelson/git
 git clone http://github.com/rickmanley-nc/laptop-configure.git
 cd /home/rnelson/git/laptop-configure
 
-ansible-playbook -i hosts main.yml --ask-sudo-pass -t firewall,myfiles,packages,libvirtd,httpd,openscap
+ansible-playbook -i hosts main.yml -t firewall,myfiles,packages,libvirtd,httpd,vpn,network
 
-sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
+cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
 source ~/.bashrc
 exit 0
