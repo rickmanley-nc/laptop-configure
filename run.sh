534a46491777d7285fc16970ad48bae82bc12257
Ricky Nelson <rnelson@laptop-rnelson-demo-com.rocketship>
2019-09-05 23:10:06 -0400

    test f30

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git clone git@github.com:rickmanley-nc/laptop-configure.git
 cd laptop-configure
 
 ansible-playbook main.yml --ask-become-pass
-ansible-playbook vpn.yml --ask-become-pass -k
+#ansible-playbook vpn.yml --ask-become-pass -k
 
 source ~/.bashrc
 exit 0
